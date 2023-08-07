e15f131a427e12dcd99115dc0adcf95b397b6133
Imdad Soomro <soomroimdad@gmail.com>
2023-08-07 17:16:51 -0400

    Update App.java

--- Java/javadeepcopy/src/main/java/com/javadevjournal/App.java
+++ Java/javadeepcopy/src/main/java/com/javadevjournal/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+                String regex = "\\[(cf|fc|c|f){1}\\]\\{(\\d+|\\*)\\}";
     }
 }
