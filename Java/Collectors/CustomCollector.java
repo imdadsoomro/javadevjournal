a0f10801976346f6388645b6622ae19b2f7a7961
Imdad Soomro <soomroimdad@gmail.com>
2023-08-14 16:20:17 -0400

    Update CustomCollector.java

--- Java/Collectors/CustomCollector.java
+++ Java/Collectors/CustomCollector.java
@@ -54,8 +54,49 @@ class StudentCollector implements Collector<Student, Student, Student> {
 
 class CustomCollector {
     public static void main(String[] args) {
+
+        List<CustomerOfferStatus> offerList = new ArrayList<>();
+        offerList.add(new CustomerOfferStatus(new Date(), "NEW"));
+        offerList.add(new CustomerOfferStatus(new Date(System.currentTimeMillis() - 3600000), "NEW"));
+        offerList.add(new CustomerOfferStatus(new Date(), "SERVED"));
+        offerList.add(new CustomerOfferStatus(new Date(System.currentTimeMillis() - 7200000), "SERVED"));
+        offerList.add(new CustomerOfferStatus(new Date(), "ACTIVATED"));
+        offerList.add(new CustomerOfferStatus(new Date(System.currentTimeMillis() - 1800000), "ACTIVATED"));
+
+        Map<String, CustomerOfferStatus> filteredOfferMap = filterAndRetrieveMostRecent(offerList);
+        List<CustomerOfferStatus> filteredOfferList = new ArrayList<>(filteredOfferMap.values());
+
+        System.out.println(filteredOfferList);
+
+        //-----
         Stream<Student> studentStream = Stream.of(new Student(30), new Student(40), new Student(50), new Student(80));
         System.out.println(studentStream.collect(new StudentCollector()).marks);
 
+    
+            List<CustomerOfferStatus> offerList = new ArrayList<>();
+
+        // Add your CustomerOfferStatus objects to the offerList
+
+        Map<String, CustomerOfferStatus> filteredOfferMap = filterAndRetrieveMostRecent(offerList);
+
+        List<CustomerOfferStatus> filteredOfferList = new ArrayList<>(filteredOfferMap.values());
+        System.out.println(filteredOfferList);
     }
+
+        public static Map<String, CustomerOfferStatus> filterAndRetrieveMostRecent(List<CustomerOfferStatus> offerList) {
+        return offerList.stream()
+                .collect(Collectors.toMap(
+                        CustomerOfferStatus::getOfferStatusName,
+                        customerOfferStatus -> customerOfferStatus,
+                        (existing, replacement) -> {
+                            if (existing.getOfferStatusChangeTimeStamp().compareTo(replacement.getOfferStatusChangeTimeStamp()) > 0) {
+                                return existing;
+                            }
+                            return replacement;
+                        }
+                ));
+    }
+
+    
+}
 }
