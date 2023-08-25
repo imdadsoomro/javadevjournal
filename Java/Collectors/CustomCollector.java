9b46b2538775881ae11c31a5c592fda1072e0d14
Imdad Soomro <soomroimdad@gmail.com>
2023-08-25 14:11:35 -0400

    Update CustomCollector.javaterm

--- Java/Collectors/CustomCollector.java
+++ Java/Collectors/CustomCollector.java
@@ -55,66 +55,44 @@ class StudentCollector implements Collector<Student, Student, Student> {
 class CustomCollector {
     public static void main(String[] args) {
 
-        List<CustomerOfferStatus> offerList = new ArrayList<>();
-        offerList.add(new CustomerOfferStatus(new Date(), "NEW"));
-        offerList.add(new CustomerOfferStatus(new Date(System.currentTimeMillis() - 3600000), "NEW"));
-        offerList.add(new CustomerOfferStatus(new Date(), "SERVED"));
-        offerList.add(new CustomerOfferStatus(new Date(System.currentTimeMillis() - 7200000), "SERVED"));
-        offerList.add(new CustomerOfferStatus(new Date(), "ACTIVATED"));
-        offerList.add(new CustomerOfferStatus(new Date(System.currentTimeMillis() - 1800000), "ACTIVATED"));
-
-        Map<String, CustomerOfferStatus> filteredOfferMap = filterAndRetrieveMostRecent(offerList);
-        List<CustomerOfferStatus> filteredOfferList = new ArrayList<>(filteredOfferMap.values());
-
-        System.out.println(filteredOfferList);
-
-        //-----
-        Stream<Student> studentStream = Stream.of(new Student(30), new Student(40), new Student(50), new Student(80));
-        System.out.println(studentStream.collect(new StudentCollector()).marks);
-
-    
-            List<CustomerOfferStatus> offerList = new ArrayList<>();
-
-        // Add your CustomerOfferStatus objects to the offerList
-
-        Map<String, CustomerOfferStatus> filteredOfferMap = filterAndRetrieveMostRecent(offerList);
-
-        List<CustomerOfferStatus> filteredOfferList = new ArrayList<>(filteredOfferMap.values());
-        System.out.println(filteredOfferList);
-    }
-
-        public static Map<String, CustomerOfferStatus> filterAndRetrieveMostRecent(List<CustomerOfferStatus> offerList) {
-        return offerList.stream()
-                .collect(Collectors.toMap(
-                        CustomerOfferStatus::getOfferStatusName,
-                        customerOfferStatus -> customerOfferStatus,
-                        (existing, replacement) -> {
-                            if (existing.getOfferStatusChangeTimeStamp().compareTo(replacement.getOfferStatusChangeTimeStamp()) > 0) {
-                                return existing;
-                            }
-                            return replacement;
-                        }
-                ));
-    }
+        
 
     
 }
+
+    private BigDecimal calculateLifetimeEarnedAmount(CustomerAccountInfoEntity customerAccountInfo) {
+    boolean cdlxEarnedAmountTimestampValid = isValidTimestamp(customerAccountInfo.getCdlxRedeemedAmountUpdateTs(), cdlxLifetimeAmountTimestampDifference);
+    boolean figgEarnedAmountTimestampValid = isValidTimestamp(customerAccountInfo.getFiggRedeemedAmountUpdateTs(), figgLifetimeAmountTimestampDifference);
+
+    if (!cdlxEarnedAmountTimestampValid || !figgEarnedAmountTimestampValid) {
+        return null;
+    }
+
+    BigDecimal lifetimeEarnedAmount = calculateLifetimeAmount(customerAccountInfo);
+    return lifetimeEarnedAmount;
 }
 
-
-...
-
-    public static Map<String, CustomerOfferStatus> filterAndRetrieveMostRecent(List<CustomerOfferStatus> offerList) {
-        return offerList.stream()
-                .filter(offer -> offer.getOfferStatusChangeTimeStamp() != null) // Ignore objects with null offerStatusChangeTimeStamp
-                .collect(Collectors.toMap(
-                        CustomerOfferStatus::getOfferStatusName,
-                        customerOfferStatus -> customerOfferStatus,
-                        (existing, replacement) -> {
-                            if (existing.getOfferStatusChangeTimeStamp().compareTo(replacement.getOfferStatusChangeTimeStamp()) > 0) {
-                                return existing;
-                            }
-                            return replacement;
-                        }
-                ));
+private boolean isValidTimestamp(Timestamp timestamp, long maxTimestampDifference) {
+    if (timestamp != null) {
+        long timeDiff = Timestamp.from(Instant.now()).getTime() - timestamp.getTime();
+        long timeDiffMinutes = TimeUnit.MILLISECONDS.toMinutes(timeDiff);
+        log.info("Timestamp Difference: {}", timeDiffMinutes);
+        return timeDiffMinutes <= maxTimestampDifference;
     }
+    return false;
+}
+
+private BigDecimal calculateLifetimeAmount(CustomerAccountInfoEntity customerAccountInfo) {
+    if (customerAccountInfo.getCdlxRedeemedAmountUpdateTs() == null && customerAccountInfo.getFiggRedeemedAmountUpdateTs() == null) {
+        return BigDecimal.valueOf(0);
+    }
+
+    BigDecimal cdlxAmount = customerAccountInfo.getCdlxLifetimeReedemedAmount() != null ?
+                            customerAccountInfo.getCdlxLifetimeReedemedAmount() : BigDecimal.valueOf(0);
+    BigDecimal figgAmount = customerAccountInfo.getFiggLifetimeReedemedAmount() != null ?
+                            customerAccountInfo.getFiggLifetimeReedemedAmount() : BigDecimal.valueOf(0);
+
+    return cdlxAmount.add(figgAmount);
+}
+
+}
