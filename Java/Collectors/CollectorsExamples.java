f7630d6a8a3a10793ab7442440fbeeb047614803
Imdad Soomro <soomroimdad@gmail.com>
2023-12-28 13:17:35 -0500

    res updt

--- Java/Collectors/CollectorsExamples.java
+++ Java/Collectors/CollectorsExamples.java
@@ -1,1 +1,28 @@
-test
+Cigna            	        		   	                                                                                                       Feb 2022-Present
+Software Engineer
+Project: Interactions and Opportunities Data	
+Design APIs, Kafka publishers and consumers for health insurance customers
+Responsibilities:
+•	Worked on various AWS services to consume and publish partner data.
+•	Publishing and consume data from Kafka topics using AWS services including ECR, ECS, Lambda, Kinesis, SQS, Firehose, Event bridge, S3, DynamoDB, Glue DB, Crawler, Athena and more.
+•	Worked on HTTP RESTful API for partners to submit their interactions.
+•	Writing lambda functions using python to process and validate the interactions.
+•	Publish interactions via HTTP GraphQL API
+•	Working on unit test cases using pytest
+•	Maintaining existing production and non-production services with the help of AWS Cloudwatch, Cloud Insights, Splunk and other internal monitoring tools
+Technologies: Python, REST APIs, Jenkins, GitLab, AWS Services, OpenShift
+
+
+  from Ar...
+EVIR- Evernorth Interactions Repository	        		   	                                              Feb 2022-Present
+Software Engineer. 
+Project: EVIR receives Interactions published through various channels, Kafka topics and Ingest API. The Interactions data is then processed and ultimately published to OpenSearch database for the consumers to query this Interactions data via a GraphQL API.
+Responsibilities:
+●	Developed various components of the AWS infrastructure using Terraform like OpenSearch, Kinesis Firehose, Eventbridge SQS etc.
+●	Developed query API written in python and Graphql which queries the OpenSearch database effectively.
+●	Optimizing system performance, cost efficiency through various code strategies and Terraform implementations.
+●	Added multiple metrics for AWS Support
+●	Monitor AWS CloudWatch, Splunk logs and remediate any issues.
+●	Write technical stories with the implementation plan and execution.
+Technologies Python 3.7.8, Terraform, AWS, Graphql, Jenkins Kafka, Postman
+
