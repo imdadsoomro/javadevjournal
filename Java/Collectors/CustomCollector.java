6c1bf17a26145c43e74b48af1a1fab779b1e70ed
Imdad Soomro <soomroimdad@gmail.com>
2024-03-06 11:11:27 -0500

    khi

--- Java/Collectors/CustomCollector.java
+++ Java/Collectors/CustomCollector.java
@@ -7,3 +7,6 @@ sqlQuery = “Select * from tableName where colVal ”
 Select * from tableName where colVal LIKE searchTermsList.get(0) OR  colVal LIKE searchTermsList.get(1) OR  colVal LIKE searchTermsList.get(2))
 
 Please rewrite the java code so that I don’t have to write LIKE multiple times. searchTermsList can have n number of String elements.
+
+        select * from MASTER_MERCHANT_MID where MERCHANT_ID LIKE '%3291188%' OR  MERCHANT_ID LIKE '%73413885%' order by MERCHANT_ID;
+select * from MASTER_MERCHANT_MID where MERCHANT_ID ILIKE ANY ('%3291188%','%73413885%') order by MERCHANT_ID 
