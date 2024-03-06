d68f00165d16621af4973dd16561c662e02a086f
Imdad Soomro <soomroimdad@gmail.com>
2024-03-06 10:59:57 -0500

    que

--- Java/Collectors/CustomCollector.java
+++ Java/Collectors/CustomCollector.java
@@ -1,61 +1,9 @@
-import java.util.*;
-import java.util.function.BiConsumer;
-import java.util.function.BinaryOperator;
-import java.util.function.Function;
-import java.util.function.Supplier;
-import java.util.stream.Collector;
-import java.util.stream.Stream;
+I have following java code that generates snowflake query: StringBuilder sqlQuery = new StringBuilder();
+List<String> searchTermsList; List<String> conditions = searchTermsList
+        .stream().map(value-> " midList LIKE '%" + value.replace("'", "\\'") + "%'").collect(Collectors.toList());
+sqlQuery = “Select * from tableName where colVal ”
+.append(String.join(" OR ", conditions));
 
-import static java.util.stream.Collector.Characteristics.IDENTITY_FINISH;
+Select * from tableName where colVal LIKE searchTermsList.get(0) OR  colVal LIKE searchTermsList.get(1) OR  colVal LIKE searchTermsList.get(2))
 
-class Student {
-    int marks;
-
-    Student() {
-    }
-
-    public Student(int i) {
-        this.marks = i;
-    }
-
-    Student addMarks(Student s) {
-        return new Student(this.marks = this.marks + s.marks);
-    }
-
-}
-
-class StudentCollector implements Collector<Student, Student, Student> {
-    @Override
-    public Supplier<Student> supplier() {
-        return Student::new;
-    }
-
-    @Override
-    public BiConsumer<Student, Student> accumulator() {
-        return Student::addMarks;
-    }
-
-    @Override
-    public BinaryOperator<Student> combiner() {
-        return Student::addMarks;
-    }
-
-    @Override
-    public Function<Student, Student> finisher() {
-        return Function.identity();
-    }
-
-    @Override
-    public Set<Characteristics> characteristics() {
-        return EnumSet.of(IDENTITY_FINISH);
-    }
-}
-
-
-class CustomCollector {
-    public static void main(String[] args) {
-        Stream<Student> studentStream = Stream.of(new Student(30), new Student(40), new Student(50), new Student(80));
-        System.out.println(studentStream.collect(new StudentCollector()).marks);
-
-    }
-}
+Please rewrite the java code so that I don’t have to write LIKE multiple times. searchTermsList can have n number of String elements.
